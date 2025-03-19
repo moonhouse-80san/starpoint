0e6dc68534c282447805f01e5aec08f8d13a4b3f
moonhouse-80san <mbg1346@gmail.com>
2025-03-19 23:57:27 +0900

    V 1.2

--- starpoint.model.php
+++ starpoint.model.php
@@ -10,50 +10,50 @@
 class StarpointModel extends Starpoint
 {
 
-    public static function getIsRated($document_srl){
-        //회원 정보와 게시글 번호를 기준으로 이미 기존에 평점을 남겼는지 체크 합니다.
+	public static function getIsRated($document_srl){
+		//회원 정보와 게시글 번호를 기준으로 이미 기존에 평점을 남겼는지 체크 합니다.
 
-        $logged_info = Context::get('logged_info');
+		$logged_info = Context::get('logged_info');
 		if(!$logged_info){
 			return new BaseObject(-1, '잘못 된 요청입니다.');
 		}
 
-        $args = new stdClass();
-        $args->member_srl = $logged_info->member_srl;
-        $args->document_srl = $document_srl;
+		$args = new stdClass();
+		$args->member_srl = $logged_info->member_srl;
+		$args->document_srl = $document_srl;
 
-        $output = executeQuery('starpoint.getRated', $args);
+		$output = executeQuery('starpoint.getRated', $args);
 
-        return $output->data;
+		return $output->data;
 
-    }
+	}
 
-    public static function getDocumentRatedTotalPoint($document_srl){
-        //게시글에 있는 전체 평점을 평균을 내어 가져옵니다.
+	public static function getDocumentRatedTotalPoint($document_srl){
+		//게시글에 있는 전체 평점을 평균을 내어 가져옵니다.
 
-        $args = new stdClass();
-        $args->document_srl = $document_srl;
+		$args = new stdClass();
+		$args->document_srl = $document_srl;
 
-        $total_point = executeQuery('starpoint.getDocumentRatedTotalPoint', $args);
-        return $total_point->data;
-    }
+		$total_point = executeQuery('starpoint.getDocumentRatedTotalPoint', $args);
+		return $total_point->data;
+	}
 
-    public static function insertStarRate($document_srl, $star_srl){
-        $logged_info = Context::get('logged_info');
+	public static function insertStarRate($document_srl, $star_srl){
+		$logged_info = Context::get('logged_info');
 		if(!$logged_info){
 			return new BaseObject(-1, '잘못 된 요청입니다.');
 		}
 
-        $args = new stdClass();
-        $args->document_srl = $document_srl;
-        $args->member_srl = $logged_info->member_srl;
-        $args->star_rate = $star_srl;
+		$args = new stdClass();
+		$args->document_srl = $document_srl;
+		$args->member_srl = $logged_info->member_srl;
+		$args->star_rate = $star_srl;
 		$args->sort_index = 'regdate';
 
 		$result = executeQuery('starpoint.insertStarRate', $args);
 
-        return false;
+		return false;
 
-    }
+	}
 
 }
