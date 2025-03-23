038cefec240a42d23b957ff07e935c100fcf0ff4
moonhouse-80san <mbg1346@gmail.com>
2025-03-23 09:52:07 +0900

    Ver 1.4

--- starpoint.admin.controller.php
+++ starpoint.admin.controller.php
@@ -12,34 +12,43 @@ class StarpointAdminController extends Starpoint
     /**
      * @brief 모듈 설정 저장
      */
-function procStarpointAdminInsertConfig()
-{
-    // 모듈 정보 가져오기
-    $vars = Context::getRequestVars();
-    
-    // 설정 객체 생성
-    $config = new stdClass();
-    
-    // 대상 모듈 처리
-    $config->target_modules = $vars->target_modules;
-    if(!is_array($config->target_modules)) {
-        $config->target_modules = array();
+    function procStarpointAdminInsertConfig()
+    {
+        // 모듈 정보 가져오기
+        $vars = Context::getRequestVars();
+        
+        // 설정 객체 생성
+        $config = new stdClass();
+        
+        // 대상 모듈 처리
+        $config->target_modules = $vars->target_modules;
+        if(!is_array($config->target_modules)) {
+            $config->target_modules = array();
+        }
+        
+        // 설정 저장
+        $oModuleController = getController('module');
+        $output = $oModuleController->insertModuleConfig('starpoint', $config);
+        if(!$output->toBool()) {
+            return $output;
+        }
+        
+        // 캐시 재생성
+        $this->recompileCache();
+        
+        // 메시지 설정 및 리다이렉트
+        $this->setMessage('성공적으로 저장했습니다.');
+        $this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispStarpointAdminConfig'));
+        
+        return new BaseObject();
     }
     
-    // 설정 저장
-    $oModuleController = getController('module');
-    $output = $oModuleController->insertModuleConfig('starpoint', $config);
-    if(!$output->toBool()) {
-        return $output;
+    /**
+     * @brief 캐시 재생성
+     */
+    function recompileCache()
+    {
+        // 캐시 파일 삭제
+        FileHandler::removeFilesInDir('./files/cache/module_info');
     }
-    
-    // 캐시 재생성
-    $this->recompileCache();
-    
-    // 메시지 설정 및 리다이렉트
-    $this->setMessage('success_saved');
-    $this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispStarpointAdminConfig'));
-    
-    return new BaseObject();
-}
 }
--- starpoint.admin.view.php
+++ starpoint.admin.view.php
@@ -26,40 +26,42 @@ class StarpointAdminView extends Starpoint
      * @brief 관리자 설정 페이지
      */
     function dispStarpointAdminConfig()
-{
-    // 모듈 목록 불러오기
-    $oModuleModel = getModel('module');
-    $module_list = $oModuleModel->getModuleList();
-    
-    // 현재 설정된 모듈 정보 가져오기
-    $config = $this->getConfig();
-    
-    // 템플릿에 변수 설정
-    Context::set('module_list', $module_list);
-    Context::set('config', $config);
-    
-    // 템플릿 파일 지정
-    $this->setTemplateFile('config');
-    
-    return new BaseObject();
-}
-
-function getConfig()
-{
-    // 모듈 설정 불러오기
-    $oModuleModel = getModel('module');
-    $config = $oModuleModel->getModuleConfig('starpoint');
-    if(!$config) {
-        $config = new stdClass();
-    }
-    
-    // 기본값 설정
-    if(!isset($config->target_modules) || !is_array($config->target_modules)) {
-        $config->target_modules = array();
+    {
+        // 모듈 목록 불러오기
+        $oModuleModel = getModel('module');
+        $module_list = $oModuleModel->getMidList(); // getMidList 함수로 변경
+        
+        // 현재 설정된 모듈 정보 가져오기
+        $config = $this->getConfig();
+        
+        // 템플릿에 변수 설정
+        Context::set('module_list', $module_list);
+        Context::set('config', $config);
+        
+        // 템플릿 파일 지정
+        $this->setTemplateFile('config');
+        
+        return new BaseObject();
     }
 
-    return $config;
-}
+    /**
+     * @brief 모듈 설정 가져오기
+     */
+    function getConfig()
+    {
+        // 모듈 설정 불러오기
+        $oModuleModel = getModel('module');
+        $config = $oModuleModel->getModuleConfig('starpoint');
+        if(!$config) {
+            $config = new stdClass();
+        }
+        
+        // 기본값 설정
+        if(!isset($config->target_modules) || !is_array($config->target_modules)) {
+            $config->target_modules = array();
+        }
+        return $config;
+    }
     
     /**
      * @brief 관리자 메인 페이지
@@ -70,5 +72,4 @@ function getConfig()
         $this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispStarpointAdminConfig'));
         return new BaseObject();
     }
-
 }
