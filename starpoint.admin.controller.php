323afc38cdf3dc56d4ddaa2c7079b67a0176c6a7
moonhouse-80san <mbg1346@gmail.com>
2025-03-23 09:40:55 +0900

    Ver 1.4

---
+++ starpoint.admin.controller.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * 게시글 별점리뷰
+ *
+ * Copyright (c) ICETEA
+ *
+ * 관리자 컨트롤러 클래스
+ */
+class StarpointAdminController extends Starpoint
+{
+    /**
+     * @brief 모듈 설정 저장
+     */
+function procStarpointAdminInsertConfig()
+{
+    // 모듈 정보 가져오기
+    $vars = Context::getRequestVars();
+    
+    // 설정 객체 생성
+    $config = new stdClass();
+    
+    // 대상 모듈 처리
+    $config->target_modules = $vars->target_modules;
+    if(!is_array($config->target_modules)) {
+        $config->target_modules = array();
+    }
+    
+    // 설정 저장
+    $oModuleController = getController('module');
+    $output = $oModuleController->insertModuleConfig('starpoint', $config);
+    if(!$output->toBool()) {
+        return $output;
+    }
+    
+    // 캐시 재생성
+    $this->recompileCache();
+    
+    // 메시지 설정 및 리다이렉트
+    $this->setMessage('success_saved');
+    $this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispStarpointAdminConfig'));
+    
+    return new BaseObject();
+}
+}
---
+++ starpoint.admin.view.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * 게시글 별점리뷰
+ *
+ * Copyright (c) ICETEA
+ *
+ * 관리자 설정 화면 클래스
+ */
+class StarpointAdminView extends Starpoint
+{
+    /**
+     * @brief 초기화
+     */
+    function init()
+    {
+        // 템플릿 경로 설정
+        $this->setTemplatePath($this->module_path.'tpl/admin');
+        
+        // 관리자 모듈 스킨 설정
+        $this->setLayoutPath('./modules/admin/tpl');
+        $this->setLayoutFile('layout');
+    }
+
+    /**
+     * @brief 관리자 설정 페이지
+     */
+    function dispStarpointAdminConfig()
+{
+    // 모듈 목록 불러오기
+    $oModuleModel = getModel('module');
+    $module_list = $oModuleModel->getModuleList();
+    
+    // 현재 설정된 모듈 정보 가져오기
+    $config = $this->getConfig();
+    
+    // 템플릿에 변수 설정
+    Context::set('module_list', $module_list);
+    Context::set('config', $config);
+    
+    // 템플릿 파일 지정
+    $this->setTemplateFile('config');
+    
+    return new BaseObject();
+}
+
+function getConfig()
+{
+    // 모듈 설정 불러오기
+    $oModuleModel = getModel('module');
+    $config = $oModuleModel->getModuleConfig('starpoint');
+    if(!$config) {
+        $config = new stdClass();
+    }
+    
+    // 기본값 설정
+    if(!isset($config->target_modules) || !is_array($config->target_modules)) {
+        $config->target_modules = array();
+    }
+
+    return $config;
+}
+    
+    /**
+     * @brief 관리자 메인 페이지
+     */
+    function dispStarpointAdminIndex()
+    {
+        // 메인 페이지는 설정 페이지로 리다이렉트
+        $this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispStarpointAdminConfig'));
+        return new BaseObject();
+    }
+
+}
